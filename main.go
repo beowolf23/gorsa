7df021ad7d3ab4a67158169234664cf675ede73a
Mihai Ciocan <mihaiciocan44@gmail.com>
2024-10-24 17:31:53 +0300

    added encryption and decryption

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 type CryptoAlgorithm interface {
 	KeyGen(bits int)
-	Encrypt(plaintext *big.Int)
-	Decrypt(ciphertext *big.Int)
+	Encrypt(plaintext *big.Int) *big.Int
+	Decrypt(ciphertext *big.Int) *big.Int
 	GetKeyInfo()
 }
 
@@ -26,7 +26,7 @@ type KeyPair struct {
 }
 
 func (rsa *RSA) KeyGen(bits int) error {
-	p, q := generatePrimeNumbers(bits)
+	p, q := GeneratePrimeNumbers(bits)
 	n := new(big.Int).Mul(p, q)
 
 	one := big.NewInt(1)
@@ -50,6 +50,16 @@ func (rsa *RSA) KeyGen(bits int) error {
 	return nil
 }
 
+func (rsa *RSA) Encrypt(plaintext *big.Int) *big.Int {
+	ciphertext := new(big.Int).Exp(plaintext, rsa.keyPair.e, rsa.keyPair.n)
+	return ciphertext
+}
+
+func (rsa *RSA) Decrypt(ciphertext *big.Int) *big.Int {
+	plaintext := new(big.Int).Exp(ciphertext, rsa.keyPair.d, rsa.keyPair.n)
+	return plaintext
+}
+
 func (rsa *RSA) GetKeyInfo() {
 	fmt.Println("Modulus: ", rsa.keyPair.n)
 	fmt.Println("Prime 2: ", rsa.keyPair.q)
@@ -58,17 +68,28 @@ func (rsa *RSA) GetKeyInfo() {
 	fmt.Println("Private exponent: ", rsa.keyPair.d)
 }
 
-func generatePrimeNumbers(bits int) (*big.Int, *big.Int) {
+func GeneratePrimeNumbers(bits int) (*big.Int, *big.Int) {
 	p, _ := rand.Prime(rand.Reader, bits)
 	q, _ := rand.Prime(rand.Reader, bits)
 	return p, q
 }
 
+func ConvertStringToBigInt(text string) *big.Int {
+	bytes := []byte(text)
+	n := new(big.Int).SetBytes(bytes)
+	return n
+}
+
+func ConvertBigIntToString(num *big.Int) string {
+	return string(num.Bytes())
+}
+
 func main() {
 
 	var bits int = 512
-
 	rsa := new(RSA)
 	rsa.KeyGen(bits)
-	rsa.GetKeyInfo()
+	ciphertext := rsa.Encrypt(ConvertStringToBigInt("something in here"))
+	plaintext := ConvertBigIntToString(rsa.Decrypt(ciphertext))
+	fmt.Println(plaintext)
 }
